094be9fccfffc71ac353d5b709c4b81c959e02e5
gustavo <gustavogarciaci1@gmail.com>
2024-05-26 15:05:34 -0300

    feat: database initial seed

---
+++ prisma/seed.ts
@@ -0,0 +1,50 @@
+import { PrismaClient } from "@prisma/client"
+import { hash } from "bcryptjs"
+import { env } from "src/env"
+
+const prisma = new PrismaClient()
+
+async function main() {
+  await prisma.organization.deleteMany()
+  await prisma.user.deleteMany()
+  await prisma.products.deleteMany()
+
+  const baseUser = await prisma.user.create({
+    data: {
+      email: "padaria@supereconomico.com",
+      name: "Padaria",
+      passwordHash: await hash(env.BASE_ORGANIZATION_PASSWORD, 8)
+    }
+  })
+
+  await prisma.organization.create({
+    data: {
+      name: "Super Econômico",
+      description: "Supermercado Econômico",
+      owner: {
+        connect: {
+          id: baseUser.id
+        }
+      },
+      members: {
+        create: {
+          user: {
+            connect: {
+              id: baseUser.id
+            }
+          },
+          role: "ADMIN"
+        }
+      }
+    }
+  })
+}
+
+async function seed() {
+  await main()
+  await prisma.$disconnect()
+}
+
+seed().then(() => {
+  console.log("Database seeded!")
+})
