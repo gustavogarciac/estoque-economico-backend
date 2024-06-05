73a45b3e75a88b3170c7b283b7ef1ace584e4b29
gustavo <gustavogarciaci1@gmail.com>
2024-06-04 21:37:35 -0300

    feat: auto join when domain matches on register user route

--- src/http/controllers/register-user.ts
+++ src/http/controllers/register-user.ts
@@ -1,43 +1,50 @@
-import { hash } from "bcryptjs";
-import { FastifyInstance } from "fastify";
-import { ZodTypeProvider } from "fastify-type-provider-zod";
-import { prisma } from "src/lib/prismadb";
-import z from "zod";
-import { BadRequestError } from "../_errors/bad-request-error";
+import { hash } from 'bcryptjs'
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { prisma } from 'src/lib/prismadb'
+import z from 'zod'
+
+import { BadRequestError } from '../_errors/bad-request-error'
 
 export async function registerUserRoute(app: FastifyInstance) {
-  app.withTypeProvider<ZodTypeProvider>()
-    .post("/users", {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/users',
+    {
       schema: {
-        summary: "Register a new user",
+        summary: 'Register a new user',
         tags: ['users'],
         body: z.object({
           email: z.string().email(),
           name: z.string(),
-          password: z.string().min(6)
+          password: z.string().min(6),
         }),
         response: {
           201: z.object({
-            userId: z.string().uuid()
-          })
-        }
-      }
-    }, async (req, reply) => {
-
-      const {
-        email,
-        name,
-        password
-      } = req.body
+            userId: z.string().uuid(),
+          }),
+        },
+      },
+    },
+    async (req, reply) => {
+      const { email, name, password } = req.body
 
       const emailAlreadyInUse = await prisma.user.findFirst({
         where: {
-          email
-        }
+          email,
+        },
       })
 
-      if(emailAlreadyInUse) {
-        throw new BadRequestError("Email already in use.")
+      const [, domain] = email.split('@')
+
+      const autoJoinOrganization = await prisma.organization.findFirst({
+        where: {
+          domain,
+          shouldAttachUsersByDomain: true,
+        },
+      })
+
+      if (emailAlreadyInUse) {
+        throw new BadRequestError('Email already in use.')
       }
 
       const passwordHash = await hash(password, 8)
@@ -47,12 +54,20 @@ export async function registerUserRoute(app: FastifyInstance) {
           email,
           name,
           passwordHash,
-        }
+          member_on: autoJoinOrganization
+            ? {
+                create: {
+                  organizationId: autoJoinOrganization.id,
+                  role: 'MEMBER',
+                },
+              }
+            : undefined,
+        },
       })
 
       return reply.status(201).send({
-        userId
+        userId,
       })
-
-    })
+    },
+  )
 }
